2062514c11bbadc4c3a79a897917f6681e349709
Lucas Souza <lucasvsouza28@gmail.com>
2025-08-02 09:10:25 -0300

    feat(nvim): disable swap file

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,7 +1,2 @@
--- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
-
--- vim.keymap.set("n", "<leader>lp", vim.diagnostics.open_float, { desc = "Open float diagnostics" })
+-- opts
+vim.o.swapfile = false
