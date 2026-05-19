4b9d712f352a619ffd7291d45aeaf987cf1702fa
Lucas Souza <lucasvsouza28@gmail.com>
2026-05-19 08:29:46 -0300

    fix(zsh/aliases): batcat fallback

--- zsh/.config/zsh/.zshrc.d/aliases.zsh
+++ zsh/.config/zsh/.zshrc.d/aliases.zsh
@@ -3,7 +3,12 @@
 # .aliases - Set whatever shell aliases you want.
 #
 
-alias bat="batcat"
+if command -v bat >/dev/null 2>&1; then
+  alias cat='bat'
+elif command -v batcat >/dev/null 2>&1; then
+  alias bat="batcat"
+  alias cat='batcat'
+fi
 
 alias la='eza -lahg --icons --group-directories-first' # eza l=long, a=all, h=header,=group
 alias tree='eza --tree --icons --group-directories-first'
